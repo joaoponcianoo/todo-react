7c9cf94e7e9249c8528f0ed8032498eb3eb4ff0a
joaoponcianoo <joaoggponciano@gmail.com>
2025-01-02 19:47:38 -0300

    Initial Commit

---
+++ src/components/AddTask.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import Input from "./Input";
+
+function AddTask({ onAddTaskSubmit }) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  return (
+    <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
+      <Input
+        type="text"
+        placeholder="Task Title"
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
+      />
+      <Input
+        type="text"
+        placeholder="Task Description"
+        value={description}
+        onChange={(event) => setDescription(event.target.value)}
+      />
+      <button
+        onClick={() => {
+          if (!title.trim() || !description.trim()) {
+            alert("Please fill in all fields");
+            return;
+          }
+          onAddTaskSubmit(title, description);
+          setTitle("");
+          setDescription("");
+        }}
+        className="bg-slate-500 text-white px-4 py-2 rounded-md"
+      >
+        Add
+      </button>
+    </div>
+  );
+}
+
+export default AddTask;
